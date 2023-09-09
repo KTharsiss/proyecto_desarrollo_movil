43a9a008936874a89d01ccfc6fd8264e5e721234
KTharsiss <lianmiguel1@gmail.com>
2023-09-09 14:07:53 -0500

    Avance Laboratorio

--- lib/main.dart
+++ lib/main.dart
@@ -24,27 +24,61 @@ class LoginApp extends StatelessWidget {
 
 class MyHomeLoginApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Form(
-      child: Column(
-          children:<Widget>[
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50), // Margen horizontal con respecto a los bordes de la pantalla
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 40),
+            Image.asset(
+              'assets/imagenes/Logo_SM.png',
+              width: 280,
+              height: 280,
+            ),
             SizedBox(height: 40),
             TextFormField(
               decoration: InputDecoration(
-                  labelText: 'Correo Institucional'
+                labelText: 'Correo Institucional',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
+            SizedBox(height: 30),
             TextFormField(
               obscureText: true,
               decoration: InputDecoration(
-                  labelText: 'Contraseña'
+                labelText: 'Contraseña',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
+            //BOTON
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 80,horizontal: 120),
-              child: ElevatedButton(onPressed: (){},child: Text('Iniciar Sesión'),),
+              padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 120),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(500, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  primary: Color(0xFF6C181B),
+                ),
+                child: Text(
+                  'INICIAR SESIÓN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Lato',
+                  ),
+                ),
+              ),
             )
-          ]
+          ],
+        ),
       ),
     );
   }
